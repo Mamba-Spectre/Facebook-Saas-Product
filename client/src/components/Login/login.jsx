8e2ce58057c4437e72fa79b16a6cb446d3160162
Harsh <harshvardhan23.007@gmail.com>
2024-03-30 22:15:37 +0530

    theme

--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -37,7 +37,7 @@ const AuthForm = () => {
         email,
         password
       };
-      const response = await axios.post(`http://localhost:8080/auth/login`, authData);
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, authData);
       if (response?.data?.facebookAuthTokens) {
         // document.cookie = "common-auth=" + encodeURIComponent(sessionToken) + "; path=/";
         // cokkieClient.save('common-auth', response?.data?.facebookAuthTokens?.sessionToken, {path:'/'})
